60c07bfb0b1dc54ca742832966500c323f5ea297
Vvatte <vvatte@yahoo.com>
2024-02-26 09:54:48 -0300

    tarefa mod 7

---
+++ tarefa7/src/Caderno.java
@@ -0,0 +1,33 @@
+//tarefa da EBAC mod 7
+
+public class Caderno {
+    private String folhas;
+
+    private float tamanho;
+
+    private String cor;
+
+    public String getFolhas() {
+        return folhas;
+    }
+
+    public void setFolhas(String folhas) {
+        this.folhas = folhas;
+    }
+
+    public float getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(float tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
